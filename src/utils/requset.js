ba3ab70e49a4e4976d9340aa073808882a53dd0e
@liyi98 <673317908@qq.com>
2020-05-30 23:02:44 +0800

    删除多余空行

--- src/utils/requset.js
+++ src/utils/requset.js
@@ -1,38 +1,44 @@
-import { Message } from 'element-ui';
-import axios from 'axios'
+import { Message } from "element-ui";
+import axios from "axios";
 // const axios = axios.create({
 //   baseUrl: BASEURL,
 //   timeout: 1000
 // });
-axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? '' : '/devapi'
+axios.defaults.baseURL = process.env.NODE_ENV === "production" ? "" : "/devapi";
 // const BASEURL = process.env.NODE_ENV === 'production' ? '' : '/devapi'
 // 添加请求拦截器
-axios.interceptors.request.use(function (config) {
-  // 在发送请求之前做些什么
-  return config;
-}, function (error) {
-  // 对请求错误做些什么
-  return Promise.reject(error);
-});
+axios.interceptors.request.use(
+  function(config) {
+    // 在发送请求之前做些什么
+    return config;
+  },
+  function(error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  }
+);
 
 // 添加响应拦截器
-axios.interceptors.response.use(function (response) {
-  // 对响应数据做点什么
-  // console.log(response)
-  let data=response.data
-  console.log(data)
-  if(data.resCode!=0) {
-    Message({
-      message:data.message,
-      type:'danger'
-    })
-    return Promise.reject(data)
-  }else {
-    return response;
+axios.interceptors.response.use(
+  function(response) {
+    // 对响应数据做点什么
+    // console.log(response)
+    let data = response.data;
+    console.log(data);
+    if (data.resCode != 0) {
+      Message({
+        message: data.message,
+        type: "danger"
+      });
+      return Promise.reject(data);
+    } else {
+      return response;
+    }
+  },
+  function(error) {
+    // 对响应错误做点什么
+    return Promise.reject(error);
   }
-}, function (error) {
-  // 对响应错误做点什么
-  return Promise.reject(error);
-});
+);
 
-export default axios
+export default axios;
