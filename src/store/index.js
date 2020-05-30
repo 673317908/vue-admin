8155dd0e39964e621ed6e14b14ff08fe00e1bed8
@liyi98 <673317908@qq.com>
2020-05-30 23:04:41 +0800

    登陆后存储用户名和token；将首页布局状态保存；获取存储的用户信息

--- src/store/index.js
+++ src/store/index.js
@@ -1,40 +1,47 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import Cookies from 'cookies-js'
+// import Cookies from "cookies-js";
 import { Login } from "../api/login";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    isCollapse: JSON.parse(sessionStorage.getItem('isCollapse')) ||false
+    isCollapse: JSON.parse(sessionStorage.getItem("isCollapse")) || false,
+    userInfo: {
+      username: sessionStorage.getItem("userName") || "",
+      token: sessionStorage.getItem("loginToken") || ""
+    }
   },
   mutations: {
+    // 改变首页布局
     controlCollapse(state) {
-      this.state.isCollapse = !this.state.isCollapse
-      sessionStorage.setItem('isCollapse', JSON.stringify(this.state.isCollapse))
-      console.log(this.state.isCollapse)
+      // console.log(state);
+      this.state.isCollapse = !this.state.isCollapse;
+      sessionStorage.setItem(
+        "isCollapse",
+        JSON.stringify(this.state.isCollapse)
+      );
+      console.log(this.state.isCollapse);
     }
   },
   actions: {
+    // 登陆按钮
     login(content, loginInfo) {
       return new Promise((resolve, reject) => {
-        Login(loginInfo).then(response => {
-          resolve(response)
-        }).catch(error => {
-          reject(error)
-        })
-      })
+        Login(loginInfo)
+          .then(response => {
+            // console.log(response);
+            let loginToken = response.data.data;
+            let userName = loginToken.username;
+            sessionStorage.setItem("userName", userName);
+            sessionStorage.setItem("loginToken", loginToken.token);
+            resolve(response);
+          })
+          .catch(error => {
+            reject(error);
+          });
+      });
     }
   },
-  modules: {
-    appss:{
-      state:{},
-      mutations:{
-        cccc(){
-          console.log(111)
-        }
-      },
-      actions:{}
-      }
-    }
+  modules: {}
 });
