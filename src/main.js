b3979579eb49216095556550eb32f3456cde3c0c
@liyi98 <673317908@qq.com>
2020-05-30 23:08:19 +0800

    封装提醒组件

--- src/main.js
+++ src/main.js
@@ -2,16 +2,19 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import axios from 'axios'
-// 引入elementui
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-// 重置样式
-import "./styles/reset.css"
-// 引入elementui修改样式
-import "./styles/elementui.scss"
+import axios from "axios";
 
-Vue.use(axios)
+import "@/router/befend.js";
+// 引入elementui
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+// 重置样式
+import "./styles/reset.css";
+// 引入elementui修改样式
+import "./styles/elementui.scss";
+import global from "@/utils/global.js";
+Vue.use(global);
+Vue.use(axios);
 Vue.use(ElementUI);
 
 // Vue.prototype.$axios=axios
