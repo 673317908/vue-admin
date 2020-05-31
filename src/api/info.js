d5b7656a599b8ff7e3217403694d992404077d6f
@liyi98 <673317908@qq.com>
2020-05-31 22:59:31 +0800

    添加修改、删除、编辑api

--- src/api/info.js
+++ src/api/info.js
@@ -7,3 +7,30 @@ export function getTitle(data) {
     data
   });
 }
+
+// 获取信息分类
+export function getCategoryAll(data) {
+  return axios.request({
+    method: "post",
+    url: "/news/getCategoryAll/",
+    data
+  });
+}
+
+// 删除分类
+export function DeleteCategory(data) {
+  return axios.request({
+    method: "post",
+    url: "/news/deleteCategory/",
+    data
+  });
+}
+
+// 编辑信息分类
+export function EditCategory(data) {
+  return axios.request({
+    method: "post",
+    url: "/news/editCategory/",
+    data
+  });
+}
