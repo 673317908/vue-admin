4b4fbac229d463c0c1cf56e15f362213884f7848
@liyi98 <673317908@qq.com>
2020-05-31 16:57:59 +0800

    获取缓存的用户名

--- src/utils/befend/api.js
+++ src/utils/befend/api.js
@@ -1,3 +1,9 @@
+// 获取token
 export function getToken() {
   return sessionStorage.getItem("loginToken");
+}
+
+// 获取用户名
+export function getUserName() {
+  return sessionStorage.getItem("userName");
 }
