7b60533624893e188d1a9f9201fd3f55cbb935dc
@liyi98 <673317908@qq.com>
2020-06-22 00:24:41 +0800

    获取省、市、区、获取角色、添加用户api

--- src/api/user.js
+++ src/api/user.js
@@ -1,9 +1,27 @@
-import axios from "axios"
+import axios from "axios";
 // 获取省、市、区
-export function getAddress(data){
-    return axios.request({
-        url:"/cityPicker/",
-        method:"post",
-        data
-    })
+export function getAddress(data) {
+  return axios.request({
+    url: "/cityPicker/",
+    method: "post",
+    data
+  });
+}
+
+// 获取角色
+export function getRole(data) {
+  return axios.request({
+    url: "/role/",
+    method: "post",
+    data
+  });
+}
+
+// 添加用户
+export function addUser(data) {
+  return axios.request({
+    url: "/user/add/",
+    method: "post",
+    data
+  });
 }
